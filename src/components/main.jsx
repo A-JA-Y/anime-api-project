0e4a37c051641de93e85b2bb7cb252c183caf603
Ajay <mailtoajay7007@gmail.com>
2024-08-22 11:51:54 +0530

    adj

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -30,11 +30,11 @@ return (
                 ) : (
                     <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-5 p-6">
                         {anime.map((anime) => (
-                            <div key={anime.id} className="card hover:scale-95 transition-all m-2 overflow-hidden bg-gray-200  rounded-lg shadow-lg relative">
+                            <div key={anime.id} className="card cursor-pointer hover:scale-95 transition-all m-2 overflow-hidden bg-gray-200  rounded-lg shadow-lg relative">
                                 <img src={anime.images.jpg.image_url} className='h-full w-full rounded-md ' alt="" />
-                                <div className="absolute bg-zinc-800 bg-opacity-75 text-white top-0 left-0 w-full h-full flex flex-col justify-center items-center opacity-0 hover:opacity-100 hover:text-2xl transition-all">
+                                <div className="absolute bg-zinc-800 bg-opacity-75 p-6 text-white top-0 left-0 w-full h-full flex flex-col justify-center items-center opacity-0 hover:opacity-100 hover:text-2xl transition-all">
                                     <h2 className='text-2xl font-serif font-semibold p-2'>{anime.title}</h2>
-                                    <p>{anime.genres.map(i=> i.name+" ")}</p>
+                                    <p className='font-serif font-thin text-wrap p-2'>{anime.genres.map(i=> i.name+" ")}</p>
                                 </div>
                             </div>
                         ))}

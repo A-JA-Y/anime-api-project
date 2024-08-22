c0f5b683c26d0ec562e6386e7717f70b85264e0b
Ajay <mailtoajay7007@gmail.com>
2024-08-22 10:55:50 +0530

    Added loader

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -10,6 +10,9 @@ function Main() {
             console.log(data.data);
             setAnime(data.data);
         })
+        .catch(error => {
+            console.log(error);
+        });
     },[]);
 
 
@@ -20,17 +23,23 @@ return (
         <div className="">
             <h1 className='text-6xl font-thin my-3 text-center drop-shadow-lg shadow-red-500'>AniWatcher</h1>
             <div>
-                <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-5 p-6">
-                    {anime.map((anime) => (
-                        <div key={anime.id} className="card m-2 overflow-hidden bg-gray-200 rounded-lg shadow-lg relative">
-                            <img src={anime.images.jpg.image_url} className='h-full w-full rounded-md hover:scale-105 transition-all' alt="" />
-                            <div className="absolute bg-zinc-800 bg-opacity-75 text-white top-0 left-0 w-full h-full flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity">
-                                <h2 className='text-xl font-serif font-semibold p-2'>{anime.title}</h2>
-                                <p>{anime.genres.map(i=> i.name+" ")}</p>
+                {anime.length === 0 ? (
+                    <div className="flex justify-center items-center h-64">
+                        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+                    </div>
+                ) : (
+                    <div className="grid lg:grid-cols-3 sm:grid-cols-1 gap-5 p-6">
+                        {anime.map((anime) => (
+                            <div key={anime.id} className="card hover:scale-95 transition-all m-2 overflow-hidden bg-gray-200  rounded-lg shadow-lg relative">
+                                <img src={anime.images.jpg.image_url} className='h-full w-full rounded-md ' alt="" />
+                                <div className="absolute bg-zinc-800 bg-opacity-75 text-white top-0 left-0 w-full h-full flex flex-col justify-center items-center opacity-0 hover:opacity-100 hover:text-2xl transition-all">
+                                    <h2 className='text-2xl font-serif font-semibold p-2'>{anime.title}</h2>
+                                    <p>{anime.genres.map(i=> i.name+" ")}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     </>
